d664de0e364516f2e4df0ed3dbaea27a5696d92f
Matthew E. Aiello-Lammens <matt.lammens@gmail.com>
2019-03-11 06:56:46 +0000

    version 0.1.0.1

--- inst/doc/spThin_vignette.R
+++ inst/doc/spThin_vignette.R
@@ -1,7 +1,5 @@
 ## ----load_package--------------------------------------------------------
 
-## Install package from source, then load package into workspace
-install.packages( type = "source", pkgs = "spThin_0.1.0.tar.gz", repos = NULL )
 library( spThin )
 
 
